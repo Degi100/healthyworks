cab13752ec0f7083ac438253f044d37cbd47774f
Rene Degering <r.degering@mail.de>
2023-07-14 07:57:04 +0200

    install react via vite, install sass, resest all

---
+++ hw-frontend/src/app.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'preact/hooks'
+import preactLogo from './assets/preact.svg'
+import viteLogo from '/vite.svg'
+import './app.scss'
+
+export function App() {
+ 
+  return (
+    <>
+      <div>
+        Here We Go !
+     </div>
+    </>
+  )
+}
