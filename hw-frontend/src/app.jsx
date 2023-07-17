1120280f410f434e58a0e6f7d8a7f82ea4af159b
Rene Degering <r.degering@mail.de>
2023-07-17 19:44:03 +0200

    create RouterNavigationRoutes Component

--- hw-frontend/src/app.jsx
+++ hw-frontend/src/app.jsx
@@ -1,32 +1,15 @@
 import "./app.scss";
-import { Routes, Route } from "react-router-dom";
-import { RouterNavigation } from "./Components/Navigation/RouterNavigation";
+import { RouterNavigationLinks } from "./Components/Navigation/RouterNavigationLinks";
+import { RouterNavigationRoutes } from "./Components/Navigation/RouterNavigationRoutes";
 
-import { HealthymanagementPage } from "./pages/HealthymanagementPage";
-import { ImpressumPage } from "./pages/ImpressumPage";
-import { KontaktPage } from "./pages/KontaktPage";
-import { NonsmokePage } from "./pages/NonsmokePage";
-import { RelextrainePage } from "./pages/RelextrainePage";
-import { StartPage } from "./pages/StartPage";
-import { StresstrainePage } from "./pages/StresstrainePage";
-import { Not__Found as Error404} from "./pages/Not__Found";
 
 
 export const App = () => {
   return (
     <>
       <div className="NavRoutes">
-        <RouterNavigation />
-        <Routes>
-          <Route path="/" element={<StartPage />} />
-          <Route path="/healthymanagement" element={<HealthymanagementPage />} />
-          <Route path="/impressum" element={<ImpressumPage />} />
-          <Route path="/kontakt" element={<KontaktPage />} />
-          <Route path="/nonsmoke" element={<NonsmokePage />} />
-          <Route path="/relextraine" element={<RelextrainePage />} />
-          <Route path="/stresstraine" element={<StresstrainePage />} />
-          <Route path="*" element={<Error404 />} />
-        </Routes>
+        <RouterNavigationLinks />
+        <RouterNavigationRoutes />
       </div>
     </>
   );
--- hw-frontend/src/main.jsx
+++ hw-frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { render } from "preact";
 import { App } from "./app.jsx";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
 render(
   <BrowserRouter>
