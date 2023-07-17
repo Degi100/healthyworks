3ccb2efca0237b3fdf9018856f7a8899587d2997
Rene Degering <r.degering@mail.de>
2023-07-17 21:05:23 +0200

    add responsive media queries

--- hw-frontend/src/app.jsx
+++ hw-frontend/src/app.jsx
@@ -1,6 +1,7 @@
 import { RouterNavigationLinks } from "./Components/Navigation/RouterNavigationLinks";
 import { RouterNavigationRoutes } from "./Components/Navigation/RouterNavigationRoutes";
 import "./app.scss";
+import "./media-breakpoints.scss";
 
 export const App = () => {
   return (
