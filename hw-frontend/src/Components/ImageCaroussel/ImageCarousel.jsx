dd43e3394bb08f96d4be4bdba31f7f55d91717ac
Rene Degering <r.degering@mail.de>
2023-07-22 14:52:43 +0200

    add image Slider

---
+++ hw-frontend/src/Components/ImageCaroussel/ImageCarousel.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import Img1 from "../../assets/bird.jpg"
+import Img2 from "../../assets/leaves.jpg"
+import Img3 from "../../assets/lion.png"
+import Img4 from "../../assets/vegetables2.jpg"
+
+
+export const ImageCarousel = () => {
+  const images = [Img1, Img2, Img3, Img4];
+
+  const [currentImage, setCurrentImage] = useState(images[0]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const randomImage = images[Math.floor(Math.random() * images.length)];
+      setCurrentImage(randomImage);
+    }, 2000);
+    return () => clearInterval(intervalId);
+  }, [images]);
+
+  return <img src={currentImage} alt="Random Image" />;
+};
--- hw-frontend/src/Pages/StartPage.jsx
+++ hw-frontend/src/Pages/StartPage.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import "../app.scss"
-
+import { ImageCarousel } from "../Components/ImageCaroussel/ImageCarousel";
+import "../app.scss";
 
 export const StartPage = () => {
   return (
     <>
       <div>
         <h1>Herzlich Willkommen.</h1>
-        
-           <h1>Startpage</h1> 
+          <ImageCarousel />
       </div>
     </>
   );
