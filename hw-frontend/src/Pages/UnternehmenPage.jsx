3dc8045a824c9f5813858d9029de5e3dda2e0b1b
Rene Degering <r.degering@mail.de>
2023-07-22 19:31:09 +0200

    edit Unternehmen content

--- hw-frontend/src/Pages/UnternehmenPage.jsx
+++ hw-frontend/src/Pages/UnternehmenPage.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React from "react";
+import content from "../data/content.json";
+import img from '../assets/jennyjen.jpg'
 
 export const UnternehmenPage = () => {
-    return <h1>Unternehmen
-    </h1>
-}
+  return (
+    <>
+      <class className="unternehmen">
+        {content.map((content, i) => (
+          <div className="contentData" key={i}>
+            <div className="headline-unternehmen">
+              <h1>{content.unternehmen.headline}</h1>
+            </div>
+            <p>{content.unternehmen.discription}</p>
+             <img src={img} width={200}alt="Bild von Jenny Jen" />
+            
+          </div>
+        ))}
+      </class>
+
+      <div className="healthyM-wrapper">{/* <Content /> */}</div>
+    </>
+  );
+};
