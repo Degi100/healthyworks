33746aa7ef05530f27aacdce99702cdf91721fb8
Rene Degering <r.degering@mail.de>
2023-07-22 18:36:07 +0200

    readmore is running in Leistungen

--- hw-frontend/src/Pages/Leistungen.jsx
+++ hw-frontend/src/Pages/Leistungen.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 // import Content from "../Components/ReadMore";
 import content from "../data/content.json";
 
 export const Leistungen = () => {
+  const [isCategorie1, setIsCategorie1] = useState(true);
+  const [isCategorie2, setIsCategorie2] = useState(true);
+  const [isCategorie3, setIsCategorie3] = useState(true);
+  const [isCategorie4, setIsCategorie4] = useState(true);
+
   return (
     <>
       <class className="leistungen">
@@ -14,19 +19,62 @@ export const Leistungen = () => {
             <p>{content.leistungen.discription}</p>
             <div className="betriebliches-gesundheitsmanagement">
               <h2>{content.leistungen.categories.categorie1.headline}</h2>
-              <p>{content.leistungen.categories.categorie1.discription}</p>
+              <p>
+                {isCategorie1
+                  ? content.leistungen.categories.categorie1.discription.slice(
+                      0,
+                      200
+                    ) + "..."
+                  : content.leistungen.categories.categorie1.discription}
+                <button onClick={() => setIsCategorie1(!isCategorie1)}>
+                  {isCategorie1 ? "Read More" : "read Less"}
+                </button>
+              </p>
             </div>
             <div className="stressmanagement-training">
               <h2>{content.leistungen.categories.categorie2.headline}</h2>
-              <p>{content.leistungen.categories.categorie2.discription}</p>
+              <p>
+                {" "}
+                {isCategorie2
+                  ? content.leistungen.categories.categorie2.discription.slice(
+                      0,
+                      200
+                    ) + "..."
+                  : content.leistungen.categories.categorie2.discription}
+                <button onClick={() => setIsCategorie2(!isCategorie2)}>
+                  {isCategorie2 ? "Read More" : "read Less"}
+                </button>
+              </p>
             </div>
             <div className="entspannung-training">
               <h2>{content.leistungen.categories.categorie3.headline}</h2>
-              <p>{content.leistungen.categories.categorie3.discription}</p>
+              <p>
+                {" "}
+                {isCategorie3
+                  ? content.leistungen.categories.categorie3.discription.slice(
+                      0,
+                      200
+                    ) + "..."
+                  : content.leistungen.categories.categorie3.discription}
+                <button onClick={() => setIsCategorie3(!isCategorie3)}>
+                  {isCategorie3 ? "Read More" : "read Less"}
+                </button>
+              </p>
             </div>
             <div className="rauchfrei-programm">
               <h2>{content.leistungen.categories.categorie4.headline}</h2>
-              <p>{content.leistungen.categories.categorie4.discription}</p>
+              <p>
+                {" "}
+                {isCategorie4
+                  ? content.leistungen.categories.categorie4.discription.slice(
+                      0,
+                      200
+                    ) + "..."
+                  : content.leistungen.categories.categorie4.discription}
+                <button onClick={() => setIsCategorie4(!isCategorie4)}>
+                  {isCategorie4 ? "Read More" : "read Less"}
+                </button>
+              </p>
             </div>
           </div>
         ))}
