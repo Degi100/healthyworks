cd8cca50aaf09a9d8fb25e8a1bdf9c5f314fdd45
Rene Degering <r.degering@mail.de>
2023-07-23 02:32:22 +0200

    add logo

--- hw-frontend/src/Components/Navigation/RouterNavigationLinks.jsx
+++ hw-frontend/src/Components/Navigation/RouterNavigationLinks.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 import "../Navigation/routerNavigationLinks.scss"
 import { BsWhatsapp as Phone } from 'react-icons/bs';
 import { AiOutlineMail as Email} from 'react-icons/ai'
-import logo from '../../assets/logo.png'
+import logo from '../../assets/logoComplete.png'
 
 export const RouterNavigationLinks = () => {
     return (
         <>
             <nav>
                 <ul> 
-                    <li className="logo"> <Link to='/'><img src={logo} alt="Logo" width={25}></img>Healthy Works</Link></li>
+                    <li className="logo"> <Link to='/'><img src={logo} width={300} alt="Logo" ></img></Link></li>
                     <li> <Link to='/'><Phone /></Link></li>
                     <li> <Link to='/'><Email /></Link></li>
                     <li> <Link to='/unternehmen'>Unternehmen</Link></li>
