68096a0e9a4d8c970df805cffa92ce2f7c68dfa1
Rene Degering <r.degering@mail.de>
2023-08-05 15:20:34 +0200

    responsive

--- hw-frontend/src/Components/Navigation/RouterNavigationLinks.jsx
+++ hw-frontend/src/Components/Navigation/RouterNavigationLinks.jsx
@@ -1,32 +1,56 @@
 import { Link } from "react-router-dom";
-import { BsWhatsapp as Phone } from 'react-icons/bs';
-import { AiOutlineMail as Email} from 'react-icons/ai';
-import { GiHamburgerMenu as Burger } from 'react-icons/Gi';
-import logo from '../../assets/logoComplete.png';
-import '../../app.scss';
+import { BsWhatsapp as Phone } from "react-icons/bs";
+import { AiOutlineMail as Email } from "react-icons/ai";
+import { GiHamburgerMenu as Burger } from "react-icons/Gi";
+import logo from "../../assets/logoComplete.png";
+import "../../app.scss";
 
 export const RouterNavigationLinks = () => {
-    return (
-        <>
-            <nav>
-                <ul className="contacts-icons"> 
-                    <li className="logo"> 
-                    <Link to='/'> <img src={logo} width={300} alt="Logo" ></img></Link></li>
-                    <li> <Link to='/'><Phone /></Link></li>
-                    <li> <Link to='/'><Email /></Link></li>
-                </ul>
+  return (
+    <>
+      <nav>
+        <div className="nav-wrapper">
+            <li className="logo">
+              <Link to="/">
+                <img src={logo} width={300} alt="Logo"></img>
+              </Link>
+            </li>
+          <ul className="contacts-icons">
+            <li>
+              <Link to="/">
+                <Phone />
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <Email />
+              </Link>
+            </li>
 
-                <ul className="burger-menu-icon">
-                    <li> <Link to='/'><Burger /></Link></li>
-                <ul className="dropdown-content">
-                    <li> <Link to='/unternehmen'>Unternehmen</Link></li>
-                    <li> <Link to='/leistungen'>Leistungen</Link></li>
-                    <li> <Link to='/impressum'>Impressum</Link></li>
-                    <li> <Link to='/kontakt'>Kontakt</Link></li>
-                </ul>
-                </ul>
-
-            </nav>
-        </>
-    )
-}
+          <ul className="burger-menu-icon">
+            <li>
+              <Link to="/">
+                <Burger />
+              </Link>
+            </li>
+            <ul className="dropdown-content">
+              <li>
+                <Link to="/unternehmen">Unternehmen</Link>
+              </li>
+              <li>
+                <Link to="/leistungen">Leistungen</Link>
+              </li>
+              <li>
+                <Link to="/impressum">Impressum</Link>
+              </li>
+              <li>
+                <Link to="/kontakt">Kontakt</Link>
+              </li>
+            </ul>
+          </ul>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
